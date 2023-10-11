f2e55315f0c26c33aa86012a9ee9621a48f49616
Bouhachlaf Mohammed <bouhachlafmohammed1234@gmail.com>
2023-10-11 17:35:49 +0200

    modification

--- main.py
+++ main.py
@@ -1,44 +1,87 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel 
 
 app = FastAPI()
 
 
-class User(BaseModel):
+clients = []
+hotel_rooms = []
+reservations = []
+
+class Client(BaseModel):
     id: int
-    username: str
+    name: str
     email: str
 
-class Article(BaseModel):
+class HotelRoom(BaseModel):
     id: int
-    title: str
-    content: str
+    room_number: int
+    capacity: int
+
+class HotelReservation(BaseModel):
+    id: int
+    client_id: int
+    room_id: int
+    check_in_date: str
+    check_out_date: str
 
 
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
+
+@app.post("/clients/")
+def create_client(client: Client):
+    clients.append(client)
+    return client
+
+@app.get("/clients/{client_id}")
+def get_client(client_id: int):
+    for client in clients:
+        if client.id == client_id:
+            return client
+    raise HTTPException(status_code=404, detail="Client not found")
+
+@app.post("/hotel-rooms/")
+def create_hotel_room(room: HotelRoom):
+    hotel_rooms.append(room)
+    return room
+
+@app.get("/hotel-rooms/{room_id}")
+def get_hotel_room(room_id: int):
+    for room in hotel_rooms:
+        if room.id == room_id:
+            return room
+    raise HTTPException(status_code=404, detail="Hotel room not found")
+
+@app.post("/hotel-reservations/")
+def create_hotel_reservation(reservation: HotelReservation):
+ 
+    if reservation.client_id not in [client.id for client in clients] or reservation.room_id not in [room.id for room in hotel_rooms]:
+        raise HTTPException(status_code=404, detail="Client or hotel room not found")
     
-    user = {"id": 2, "username": "yohan", "email": "yohan@example.com"}
-    return user
+    reservations.append(reservation)
+    return reservation
 
-@app.post("/users/")
-def create_user(user: User):
-   
-    return user
+@app.get("/hotel-reservations/{reservation_id}")
+def get_hotel_reservation(reservation_id: int):
+    for reservation in reservations:
+        if reservation.id == reservation_id:
+            return reservation
+    raise HTTPException(status_code=404, detail="Reservation not found")
+
+@app.put("/hotel-reservations/{reservation_id}")
+def update_hotel_reservation(reservation_id: int, reservation: HotelReservation):
+    for i, r in enumerate(reservations):
+        if r.id == reservation_id:
+          
+            reservations[i] = reservation
+            return reservation
+    raise HTTPException(status_code=404, detail="Reservation not found")
+
+@app.delete("/hotel-reservations/{reservation_id}")
+def delete_hotel_reservation(reservation_id: int):
+    for i, reservation in enumerate(reservations):
+        if reservation.id == reservation_id:
+            del reservations[i]
+            return {"message": "Reservation deleted"}
+    raise HTTPException(status_code=404, detail="Reservation not found")
 
 
-@app.get("/articles/{article_id}")
-def get_article(article_id: int):
-    
-    
-    article= {"id": article_id, "title": "Mon article", "content": "Contenu de l'article."}
-    return article
-
-@app.post("/articles/")
-def create_article(article: Article):
-   
-    return article
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
